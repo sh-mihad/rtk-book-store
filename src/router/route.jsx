30e045a5e6bcdfa1e79b9c5bc92bcda7654f3813
Sabbir <sh.mihad.cse@gmail.com>
2024-11-10 20:06:54 +0600

    book form ui done..

--- src/router/route.jsx
+++ src/router/route.jsx
@@ -1,10 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
+import BookAddEditForm from "../../pages/AddEditBook";
 import Home from "../../pages/Home";
 
 const router = createBrowserRouter([
      {
         path:"/",
         element:<Home/>
+     },
+     {
+        path:"/addBook",
+        element:<BookAddEditForm/>
+     },
+     {
+        path:"/editBook/:id",
+        element:<BookAddEditForm/>
      }
 ])
 
